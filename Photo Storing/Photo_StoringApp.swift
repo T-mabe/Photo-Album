681c884eba1f5ff6a0199b4df3b76217a24f8e58
Abe Molina <abemolina03@gmail.com>
2023-07-27 18:10:57 -0400

    Initial Commit

---
+++ Photo Storing/Photo_StoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_StoringApp.swift
+//  Photo Storing
+//
+//  Created by Abe Molina on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Photo_StoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
